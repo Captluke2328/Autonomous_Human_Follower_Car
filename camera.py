b5f06c0994e81de0b4f289932669d3df1f3fc60f
Captluke2328 <81543946+Captluke2328@users.noreply.github.com>
2022-09-27 02:23:35 +0800

    + Added visualization

--- camera.py
+++ camera.py
@@ -17,3 +17,23 @@ class Camera:
     def close_camera(self):
         self.camera.Close()
         
+    
+    def visualise(self,img,data):
+        
+        # Draw Black Rectangle Bottom
+        cv2.rectangle(img, (0,self.height-24),(self.width, self.height),(0,0,0),-1)
+        
+        # Draw Center Middle Line
+        cv2.line(img,(self.width//2,0),(self.width//2,self.height-24), (255,0,255),3)
+        
+        # Draw Center Image
+        cv2.circle(img, (self.width // 2, self.height // 2), 5, (0, 255, 0), cv2.FILLED)
+        
+        # Draw Arrowed Line
+        cv2.arrowedLine(img, (int(self.width // 2), int(self.height // 2)), (int(data[0][0]), int(data[0][1])), (255, 0, 255), 5, 10)
+
+        return img
+
+        
+        
+        
